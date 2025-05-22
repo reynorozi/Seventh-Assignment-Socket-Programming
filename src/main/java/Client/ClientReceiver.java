b3200a14baf07b6cb561bf875e3aa39c79f77a32
Reyhane Noroozi <reyreynorozi@gmail.com>
2025-05-21 23:04:13 -0700

    Complete chatbox

--- src/main/java/Client/ClientReceiver.java
+++ src/main/java/Client/ClientReceiver.java
@@ -1,22 +1,42 @@
 package Client;
 
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientReceiver implements Runnable {
-    // TODO: Declare a variable to hold the input stream from the socket
-    public ClientReceiver() {
-        // TODO: Modify this constructor to receive either a Socket or an InputStream as a parameter
-        // TODO: Initialize the input stream variable using the received parameter
+
+    DataInputStream in;
+
+    public ClientReceiver(DataInputStream in) throws IOException {
+        this.in = in;
     }
 
     @Override
     public void run() {
         try {
             while (true) {
+
+            String messageserver = in.readUTF();
+
+                if(messageserver.startsWith("M;")){
+
+                    String b = messageserver.replace("M;","");
+                    String[] parts = b.split(",");
+                    String username = parts[0];
+                    String message = parts[1];
+                    System.out.println("[" +username+"]:"  + message);
+                }
+
+
+
                 //TODO: Listen for new messages from server
                 //TODO: print the  new message in CLI
             }
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
