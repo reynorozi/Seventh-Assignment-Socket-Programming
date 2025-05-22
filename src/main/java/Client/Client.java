840fa7dc57cb20802f09a31135e4844c8be089b6
Reyhane Noroozi <reyreynorozi@gmail.com>
2025-05-21 23:04:52 -0700

    Complete upload/download

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -5,15 +5,22 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    // TODO: Declare variables for socket input/output streams
+
+    private Socket socket;
+    static DataInputStream in;
+    static DataOutputStream out;
     private static String username;
+
     public static void main(String[] args) throws Exception {
 
         try (Socket socket = new Socket("localhost", 12345)) {
-            //TODO: Use the socket input and output streams as needed
+
 
 
             Scanner scanner = new Scanner(System.in);
+
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
             // --- LOGIN PHASE ---
             System.out.println("===== Welcome to CS Music Room =====");
@@ -22,17 +29,30 @@ public class Client {
             boolean loggedIn = false;
             while (!loggedIn) {
                 System.out.print("Username: ");
-                String username = scanner.nextLine();
+                username = scanner.nextLine();
                 System.out.print("Password: ");
                 String password = scanner.nextLine();
 
 
                 sendLoginRequest(username, password);
 
-                // TODO: Receive and check the server's login response
-                // TODO: Set 'loggedIn = true' if credentials are correct; otherwise, prompt again
-            }
 
+                String message = in.readUTF();
+                System.out.println(message);
+
+                if (message.startsWith("login:")) {
+
+                    String succes = message.split(":")[1];
+                    if (succes.equals("true")) {
+
+                        loggedIn = true;
+
+                    } else if (succes.equals("false")) {
+
+                        loggedIn = false;
+                    }
+                }
+            }
             // --- ACTION MENU LOOP ---
             while (true) {
                 printMenu();
@@ -53,6 +73,7 @@ public class Client {
 
         } catch (IOException e) {
             System.out.println("Connection error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -64,14 +85,22 @@ public class Client {
         System.out.println("0. Exit");
     }
 
-    private static void sendLoginRequest(String username, String password) {
-        //TODO: send the login request
+    private static void sendLoginRequest(String username, String password) throws IOException {
+
+
+        out.writeUTF("login,"+ username + "," + password);
+        out.flush();
+
     }
     private static void enterChat(Scanner scanner) throws IOException {
-        System.out.print("You have entered the chat ");
+        System.out.print("You have entered the chat \n");
+
+        ClientReceiver CR = new ClientReceiver(in);
+        Thread t = new Thread(CR);
+        t.start();
 
 
-        //TODO: Create and start ClientReceiver thread to continuously get new messages from server
+
         String message_string = "";
         while (!message_string.equalsIgnoreCase("/exit")){
             message_string = scanner.nextLine();
@@ -84,13 +113,15 @@ public class Client {
     }
 
     private static void sendChatMessage(String message_to_send) throws IOException {
-        //TODO: send the chat message
+
+        out.writeUTF("M," + username +"," + message_to_send);
     }
 
     private static void uploadFile(Scanner scanner) throws IOException {
 
-        //TODO: list all files in the resources/Client/<username> folder
-        File[] files = null;
+        String filepath = "src/main/resources/Client/" + username;
+        File folder = new File(filepath);
+        File[] files = folder.listFiles();
         if (files == null || files.length == 0) {
             System.out.println("No files to upload.");
             return;
@@ -116,12 +147,80 @@ public class Client {
             return;
         }
 
+
+        out.writeUTF("UPLOAD," + files[choice].getName() + "," + files[choice].length());
+        out.flush();
+
+        FileInputStream fis = new FileInputStream(files[choice]);
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+            out.flush();
+
         // TODO: Notify the server that a file upload is starting (e.g., send file metadata)
         // TODO: Read the file into a byte array and send it over the socket
     }
 
     private static void requestDownload(Scanner scanner) throws IOException {
         // TODO: Send a request to the server to retrieve the list of available files
+        out.writeUTF("DOWNLOAD");
+        String fileL = in.readUTF();
+
+        String filename = fileL.replace("SERVERFILE|","");
+        String[] filelist = filename.split(",");
+
+
+        if (filelist == null || filelist.length == 0) {
+            System.out.println("No files to download.");
+            return;
+        }
+
+
+        System.out.println("Select a file to download:");
+
+        for(int i = 0; i < filelist.length; i++){
+
+            System.out.println( (i+1) + "." +  filelist[i]);
+        }
+
+        System.out.print("Enter file number: ");
+        int choice;
+        try {
+            choice = Integer.parseInt(scanner.nextLine()) - 1;
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input.");
+            return;
+        }
+
+        if (choice < 0 || choice >= filelist.length) {
+            System.out.println("Invalid choice.");
+           return;
+        }
+
+
+        out.writeUTF("AskFile," + filelist[choice]);
+
+
+
+        long fileSize = in.readLong();
+
+        File outFile = new File("src/main/resources/Client/" + username + "/" + filelist[choice]);
+        FileOutputStream fileOutputStream = new FileOutputStream(outFile);
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        long totalRead = 0;
+
+        while (totalRead < fileSize && (bytesRead = in.read(buffer, 0, (int)Math.min(buffer.length, fileSize - totalRead))) != -1) {
+            fileOutputStream.write(buffer, 0, bytesRead);
+            totalRead += bytesRead;
+        }
+
+        fileOutputStream.close();
+        System.out.println("File received.");
+
         // TODO: Display the file names and prompt the user to select one
         // TODO: Download the selected file and save it to the user's folder in 'resources/Client/<username>'
     }
---
+++ src/main/java/Server/ClientHandler.java
@@ -0,0 +1,173 @@
+package Server;
+
+import javax.imageio.stream.FileImageInputStream;
+import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.net.Socket;
+import java.util.List;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private List<ClientHandler> allClients;
+    private String username;
+    private static File[] files;
+
+
+    public ClientHandler(Socket socket ) {
+        this.socket = socket;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            in = new DataInputStream(socket.getInputStream());
+
+
+            while (true) {
+
+                String request = in.readUTF();
+
+                if (request.startsWith("login,")) {
+
+                    String[] parts = request.split(",");
+
+                    if (parts.length >= 3) {
+                        username = parts[1];
+                        String pass = parts[2];
+                        handleLogin(username, pass);
+                    }
+                }
+                if (request.startsWith("M,")){
+
+                    String message = request.replace("M,","");
+                    System.out.println(message);
+                    broadcast(message);
+                }
+//                UPLOAD,FILENAME,FILELENGHT
+                if (request.startsWith("UPLOAD,")) {
+                    System.out.println(request.replace("UPLOAD,",""));
+                    String[] parts = request.split(",");
+                    String filename = parts[1];
+                    long length = Long.parseLong(parts[2]);
+                    receiveFile(filename,length);
+                }
+                if(request.startsWith("DOWNLOAD")){
+                    sendFileList();
+
+                }
+                if (request.startsWith("AskFile,")) {
+                    String file = request.replace("AskFile,","");
+                    sendFile(file);
+                }
+
+            }
+        }
+        catch (Exception e) {
+
+        } finally {
+
+            allClients = Server.clients;
+        }
+    }
+
+
+
+    private void sendMessage(String msg) throws IOException {
+
+        out.writeUTF("M;"+ msg);
+
+    }
+    private void broadcast(String msg) throws IOException {
+
+        String[] parts = msg.split(",");
+        String username = parts[0];
+
+
+        for (ClientHandler client : allClients) {
+            if (!client.username.equals(username)) {
+                client.sendMessage(msg);
+            }
+        }
+    }
+
+    private void sendFileList() throws IOException {
+         String file = "src/main/resources/Server/Files";
+         File folder = new File(file);
+         files = folder.listFiles();
+        StringBuilder filename = new StringBuilder("SERVERFILE|");
+        for (int i = 0; i < files.length; i++) {
+            filename.append(files[i].getName());
+            if (i != files.length - 1) filename.append(",");
+        }
+        out.writeUTF(filename.toString());
+
+        // TODO: List all files in the server directory
+        // TODO: Send a message containing file names as a comma-separated string
+    }
+    private void sendFile(String fileName) throws IOException {
+
+        File selectedfile = null;
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].getName().equals(fileName)) {
+
+                out.writeLong( files[i].length());
+
+                selectedfile = files[i];
+
+            }
+        }
+
+        FileInputStream file = new  FileInputStream(selectedfile);
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = file.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
+        }
+        out.flush();
+
+
+        // TODO: Send file name and size to client
+        // TODO: Send file content as raw bytes
+    }
+    private void receiveFile(String filename, long fileLength) throws IOException {
+
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int bytesRead = 0;
+        int totalRead = 0;
+
+        while (totalRead < fileLength &&
+                (bytesRead = in.read(buffer, 0, (int)Math.min(buffer.length, fileLength - totalRead))) != -1) {
+            baos.write(buffer, 0, bytesRead);
+            totalRead += bytesRead;
+        }
+        byte[] fileData = baos.toByteArray();
+        baos.close();
+        saveUploadedFile(filename, fileData);
+
+    }
+    private void saveUploadedFile(String filename, byte[] data) throws IOException {
+        File file = new  File ("src/main/resources/Server/Files/"+filename);
+        FileOutputStream filee = new FileOutputStream(file);
+        filee.write(data);
+        filee.close();
+    }
+
+    private void handleLogin(String username, String password) throws IOException, ClassNotFoundException {
+
+        boolean login = Server.authenticate(username, password);
+        out = new DataOutputStream(socket.getOutputStream());
+        if(login){
+            out.writeUTF("login:true");
+        }
+        else if(!login){
+            out.writeUTF("login:false");
+        }
+    }
+
+}
